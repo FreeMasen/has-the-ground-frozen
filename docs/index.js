615599ae07c342557d8b759a77d616ca6acbadb1
Robert Masen <robert.masen@smartthings.com>
2022-11-04 16:02:40 -0500

    state+stations -> zipcode

--- docs/index.js
+++ docs/index.js
@@ -1,110 +1,49 @@
 
+/**@type HTMLDivElement */
 const ANS = document.getElementById("answer");
+/**@type HTMLSummaryElement */
+const OL_TITLE = document.getElementById("observations-title");
+/**@type HTMLOLElement */
 const OL = document.getElementById("observations");
-/**@type HTMLSelectElement */
-const STATE = document.getElementById("state-select");
-const STATION = document.getElementById("station-select");
-const STATES = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL",
-    "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA",
-    "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC",
-    "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT",
-    "VA", "WA", "WV", "WI", "WY",];
-const STATE_KEY = "saved-state";
-const STATION_KEY = "saved-station";
+/**@type HTMLInputElement */
+const ZIPS = document.getElementById("zipcode");
+/**@type HTMLButtonElement */
+const CHECK_BTN = document.getElementById("check-now");
 
-STATE.addEventListener("change", async () => {
-    let state = getState();
-    if (!state || state.length == 0) {
-        localStorage.removeItem(STATE_KEY);
-    } else {
-        localStorage.setItem(STATE_KEY, state);
-    }
-    localStorage.removeItem(STATION_KEY);
-    await populateStations();
-});
-STATION.addEventListener("change", async () => {
-    let stationId = await getStationId(); 
-    if (!stationId || stationId.length == 0) {
-        localStorage.removeItem(STATION_KEY);
-        return;
-    }
-    localStorage.setItem(STATION_KEY, stationId);
-    await getObservations(stationId);
-});
+const ZIP_KEY = "saved-zip-code";
 
-function createOpt(text, value) {
-    let opt = document.createElement("option");
-    opt.innerText = text;
-    opt.value = value;
-    return opt;
-}
+const YES_CLASS = "yes";
+const NO_CLASS = "no";
+const ERROR_CLASS = "error";
 
-function populateStates() {
-    while (STATE.hasChildNodes()) {
-        STATE.removeChild(STATE.lastChild);
-    }
-    let opt = createOpt("", "")
-    STATE.appendChild(opt);
-    for (let abbr of STATES) {
-        let opt = createOpt(abbr, abbr);
-        STATE.appendChild(opt);
-    }
-}
 
-function getState() {
-    let state = STATE.selectedOptions[0]?.value;
-    if (!state || state.length == 0) {
-        localStorage.removeItem(STATE_KEY);
-        return;
-    }
-    localStorage.setItem(STATE_KEY, state);
-    return state;
-}
+import { ZIP_CODES } from "./zipCodes.js"
 
-async function populateStations(state) {
-    state = state || getState();
-    setFrozen(null);
-    while (STATION.hasChildNodes()) {
-        STATION.removeChild(STATION.lastChild);
-    }
-    if (!state || state.length == 0) {
-        return;
-    }
-    let res = await fetch(encodeURI(`https://api.weather.gov/stations?state=${state}`));
-    if (!res.ok) {
-        return console.warn("Error from request", await res.text());
-    }
-    let stations = await res.json();
-    STATION.appendChild(createOpt("", ""));
-    stations.features.sort((l, r) => {
-        let ll = l.properties.name.toLowerCase();
-        let rl = r.properties.name.toLowerCase();
-        if (ll < rl) {
-            return -1;
-        }
-        if (ll > rl) {
-            return 1;
-        }
-        return 0;
-    });
-    for (let station of stations.features) {
-        let opt = createOpt(
-            station.properties.name,
-            station.properties.stationIdentifier,
-        );
-        STATION.appendChild(opt);
-    }
-}
+ZIPS.addEventListener("change", lookupAnswer);
+CHECK_BTN.addEventListener("click", lookupAnswer);
 
-async function stationSelected() {
-    localStorage.setItem(STATION_KEY, stationId);
-    setFrozen(null);
-    let stationId = getStationId();
+async function lookupAnswer() {
+    let latLong = getLatLong();
+    let stationId = await getStationId(latLong);
     if (!stationId) {
         return;
     }
-    await getObservations();
+    await getObservations(stationId);
 }
+
+function getLatLong() {
+    let selected = ZIPS.value;
+    if (!selected || selected == "") {
+        return;
+    }
+    let zip = ZIP_CODES[selected.trim()];
+    if (!zip) {
+        return console.warn("Unknown zip code", zip)
+    }
+    localStorage.setItem(ZIP_KEY, selected.trim());
+    return `${zip.lat},${zip.long}`;
+}
+
 
 function calculateDateRange() {
     let end = new Date();
@@ -116,12 +55,31 @@ function calculateDateRange() {
     }
 }
 
-function getStationId() {
-    let stationId = STATION.selectedOptions[0]?.value;
-    if (!stationId || stationId.length == 0) {
-        return null;
+async function getStationId(latLong) {
+    latLong = latLong || getLatLong();
+    if (!latLong) {
+        return console.warn("No lat long found");
     }
-    return stationId;
+    let pointsRes = await fetch(encodeURI(`https://api.weather.gov/points/${latLong}`));
+    if (!pointsRes.ok) {
+        return console.error("Bad response to points", await pointsRes.text())
+    }
+    let info = await pointsRes.json();
+    
+    let nextUrl = info.properties?.observationStations;
+    if (!nextUrl) {
+        console.warn("No observationStations for points", latLong, info);
+    }
+    let stationsRes = await fetch(nextUrl);
+    if (!stationsRes.ok) {
+        return console.error("Bad response to observationStations", await stationsRes.text())
+    }
+    let gridStations = await stationsRes.json();
+    let station = gridStations?.features[0]?.properties?.stationIdentifier;
+    if (!station) {
+        console.warn("No grid station found: ", gridStations);
+    }
+    return station;
 }
 let timeoutId;
 function recursiveUpdate() {
@@ -135,15 +93,26 @@ async function getObservations(stationId) {
     while (OL.hasChildNodes()) {
         OL.removeChild(OL.lastChild);
     }
+    OL_TITLE.classList.remove(ERROR_CLASS);
     let { start, end } = calculateDateRange();
-    let res = await fetch(encodeURI(`https://api.weather.gov/stations/${getStationId()}/observations?start=${start}&end=${end}`));
+    let res = await fetch(encodeURI(`https://api.weather.gov/stations/${stationId}/observations?start=${start}&end=${end}`));
     if (!res.ok) {
         console.error("Error in req: ", await res.text());
         return;
     }
     let observations = await res.json();
+    let temps = observations.features.filter(obs => obs.properties?.temperature?.value)
     let frozen = true;
-    for (let obs of observations.features) {
+    if (temps.length == 0) {
+        let li = document.createElement("li");
+        let txt = document.createTextNode("No observations found, try a different station")
+        li.appendChild(txt);
+        OL.appendChild(li);
+        OL_TITLE.classList.add(ERROR_CLASS);
+        return setFrozen();
+    }
+    
+    for (let obs of temps) {
         let props = obs.properties;
         let temp = props.temperature.value;
         if (!temp) {
@@ -154,7 +123,7 @@ async function getObservations(stationId) {
         let ts = dt.toLocaleString([], { dateStyle: "short", timeStyle: "short" });
         let unitCode = props.temperature.unitCode;
         unitCode = unitCode.substring(unitCode.length - 1);
-        let li = document.createElement('li');
+        let li = document.createElement("li");
         let txt = document.createTextNode(`${ts}: ${temp}${unitCode}`);
         li.appendChild(txt);
         OL.appendChild(li)
@@ -164,32 +133,25 @@ async function getObservations(stationId) {
 
 function setFrozen(isFrozen) {
     clearTimeout(timeoutId);
-    if (isFrozen == true) {
+    if (isFrozen === true) {
         ANS.innerText = "YES!"
-        ANS.classList.add("yes");
-        ANS.classList.remove("no");
-    } else if (isFrozen == false) {
+        ANS.classList.add(YES_CLASS);
+        ANS.classList.remove(NO_CLASS);
+    } else if (isFrozen === false) {
         ANS.innerText = "NOPE";
-        ANS.classList.add("no");
-        ANS.classList.remove("yes");
+        ANS.classList.add(NO_CLASS);
+        ANS.classList.remove(YES_CLASS);
     } else {
         ANS.innerText = "?";
-        ANS.classList.remove("no");
-        ANS.classList.remove("yes");
+        ANS.classList.remove(NO_CLASS);
+        ANS.classList.remove(YES_CLASS);
     }
 }
 
-populateStates();
-
 (async () => {
-    let savedState = localStorage.getItem(STATE_KEY);
-    let savedStation = localStorage.getItem(STATION_KEY);
-    if (savedState && savedState.length > 0) {
-        STATE.value = savedState;
-        await populateStations();
-        if (savedStation && savedStation.length > 0) {
-            STATION.value = savedStation;
-            await getObservations(savedStation);
-        }
+    let savedZip = localStorage.getItem(ZIP_KEY);
+    if (savedZip && ZIP_CODES[savedZip]) {
+        ZIPS.value = savedZip;
     }
+    await lookupAnswer();
 })().catch(console.error);
